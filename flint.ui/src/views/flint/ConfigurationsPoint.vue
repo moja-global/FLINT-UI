87eb207e2b76f6b44afee94a8a4da5894a38553e
Shubham Karande <shubhamkarande13@gmail.com>
2021-07-28 05:36:17 +0530

    implement sending Point module config from UI (#17)

--- flint.ui/src/views/flint/ConfigurationsPoint.vue
+++ flint.ui/src/views/flint/ConfigurationsPoint.vue
@@ -1,129 +1,135 @@
 <template>
-  <div class="relative bg-gradient-to-r from-green-400 to-blue-500 md:pt-32 pb-32 pt-12 w-full">
+  <div
+    class="relative bg-gradient-to-r from-green-400 to-blue-500 md:pt-32 pb-32 pt-12 w-full"
+  >
     <div class="px-4 md:px-10 mx-auto w-full">
       <div>
-      <div class="bg-white p-6 rounded-lg shadow-lg">
-    <h2 class="text-2xl font-bold mb-2 text-gray-800">Point example simulation configuration</h2>
-    <p class="text-gray-700">1: Pools</p>
-  </div>
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+          <h2 class="text-2xl font-bold mb-2 text-gray-800">
+            Point example simulation configuration
+          </h2>
+          <p class="text-gray-700">1: Pools</p>
+        </div>
 
-    <div class="flex flex-wrap">
-        <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center"> 
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center">
             <div class="slider-component justify-center">
-            <h3 class="pb-4">Pool 1 configuration:</h3>
-            <div class="output">Pool 1: {{ pool1.value }} </div>
-            <Slider
-            v-model="pool1.value"
-            v-bind="pool1"
-            v-on:change="loggingVal1"
-            ></Slider>
+              <h3 class="pb-4">Pool 1 configuration:</h3>
+              <div class="output">Pool 1: {{ pool1.value }}</div>
+              <Slider
+                v-model="pool1.value"
+                v-bind="pool1"
+                v-on:change="loggingVal1"
+              ></Slider>
             </div>
-        </div>
+          </div>
 
-        <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center"> 
+          <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center">
             <div class="slider-component">
-            <h3 class="pb-4">Pool 2 configuration:</h3>
-            <div class="output">Pool 2: {{ pool2.value }} </div>
-            <Slider
-            v-model="pool2.value"
-            v-bind="pool2"
-            v-on:change="loggingVal2"
-            ></Slider>
+              <h3 class="pb-4">Pool 2 configuration:</h3>
+              <div class="output">Pool 2: {{ pool2.value }}</div>
+              <Slider
+                v-model="pool2.value"
+                v-bind="pool2"
+                v-on:change="loggingVal2"
+              ></Slider>
             </div>
-        </div>
+          </div>
 
-        <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center"> 
+          <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center">
             <div class="slider-component">
-            <h3 class="pb-4">Pool 3 configuration:</h3>
-            <div class="output">Pool 3: {{ pool3.value }} </div>
-            <Slider
-            v-model="pool3.value"
-            v-bind="pool1"
-            v-on:change="loggingVal3"
-            ></Slider>
+              <h3 class="pb-4">Pool 3 configuration:</h3>
+              <div class="output">Pool 3: {{ pool3.value }}</div>
+              <Slider
+                v-model="pool3.value"
+                v-bind="pool1"
+                v-on:change="loggingVal3"
+              ></Slider>
             </div>
-        </div>
+          </div>
 
-        <button @click="finalPoolValues" class="inline-block align-middle w-full mt-4 flex-initial bg-white hover:bg-red-500 hover:text-white text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-           <i class="fas fa-check"></i> Confirm and run with the new Pool configuration
+          <button
+            @click="finalPoolValues"
+            class="inline-block align-middle w-full mt-4 flex-initial bg-white hover:bg-red-500 hover:text-white text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          >
+            <i class="fas fa-check"></i> Confirm and run with the new Pool
+            configuration
           </button>
-        
-        <!-- <button @click="runUpdatedConfig" class="inline-block align-middle w-full mt-4 flex-initial bg-white hover:bg-blue-500 hover:text-white text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+
+          <!-- <button @click="runUpdatedConfig" class="inline-block align-middle w-full mt-4 flex-initial bg-white hover:bg-blue-500 hover:text-white text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
            <i class="far fa-play-circle"></i> Run with new Config
           </button> -->
-    </div>
-    
+        </div>
+      </div>
     </div>
   </div>
- </div>
 </template>
 
 <script>
-import Slider from '@vueform/slider'
+import Slider from "@vueform/slider";
 
 export default {
   components: {
     Slider,
   },
   data() {
-      return {
-        pool1: {
-            min: 0.0,   
-            max: 400.0,
-            step: 10.0,
-            value: 100.0
-        },
-        pool2: {
-            min: 0.0,   
-            max: 400.0,
-            step: 10.0,
-            value: 100.0
-        },
-        pool3: {
-            min: 0.0,   
-            max: 400.0,
-            step: 10.0,
-            value: 100.0
-        },
-        // configvalidated: false
-      }
-    },
+    return {
+      pool1: {
+        min: 0.0,
+        max: 400.0,
+        step: 10.0,
+        value: 100.0,
+      },
+      pool2: {
+        min: 0.0,
+        max: 400.0,
+        step: 10.0,
+        value: 100.0,
+      },
+      pool3: {
+        min: 0.0,
+        max: 400.0,
+        step: 10.0,
+        value: 100.0,
+      },
+      // configvalidated: false
+    };
+  },
   methods: {
-    loggingVal1 () {
-    //   console .log(typeof(this.pool1.value).toFixed(1));
+    loggingVal1() {
+      //   console .log(typeof(this.pool1.value).toFixed(1));
       console.log(this.pool1.value.toFixed(1));
     },
-    loggingVal2 () {
+    loggingVal2() {
       console.log(this.pool2.value.toFixed(1));
     },
-    loggingVal3 () {
+    loggingVal3() {
       console.log(this.pool3.value.toFixed(1));
     },
-    finalPoolValues () {
-       if ((this.pool1.value.toFixed(1))===(this.pool2.value.toFixed(1)) && (this.pool2.value.toFixed(1))===(this.pool3.value.toFixed(1)))
-       {
-           console.log("Config values not changed!!");
-       }
-       let pool_final = {
-           'Pool 1':'#$'+this.pool1.value.toFixed(1)+'#$', 
-           'Pool 2':'#$'+this.pool2.value.toFixed(1)+'#$', 
-           'Pool 3':'#$'+this.pool3.value.toFixed(1)+'#$'
-       };
-       console.log(pool_final);
+    finalPoolValues() {
+      if (
+        this.pool1.value.toFixed(1) === this.pool2.value.toFixed(1) &&
+        this.pool2.value.toFixed(1) === this.pool3.value.toFixed(1)
+      ) {
+        console.log("Config values not changed!!");
+      }
+
+      this.$store.commit("set_pointConfig_Pool_1", this.pool1.value.toFixed(1));
+      this.$store.commit("set_pointConfig_Pool_2", this.pool2.value.toFixed(1));
+      this.$store.commit("set_pointConfig_Pool_3", this.pool3.value.toFixed(1));
+
+      this.$store.dispatch('send_pointConfig');
     },
     // runUpdatedConfig () {
     //     console.log("Or can we do all at one go!!")
     // },
-
-  }
+  },
 };
 </script>
-
 
 <style src="@vueform/slider/themes/default.css"></style>
 
 <style>
-
 .slider-component {
   background: #ffffff;
   margin: 20px;
