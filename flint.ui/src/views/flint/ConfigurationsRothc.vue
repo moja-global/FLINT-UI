aa977b2092c376dac3acc91751b317d06afe73de
aornugent <aornugent@gmail.com>
2021-07-31 21:44:22 +1000

    Move a div

--- flint.ui/src/views/flint/ConfigurationsRothc.vue
+++ flint.ui/src/views/flint/ConfigurationsRothc.vue
@@ -3,45 +3,43 @@
   <div class="relative bg-gradient-to-r from-green-400 to-blue-500 md:pt-32 pb-32 pt-12 w-full">
     <div class="px-4 md:px-10 mx-auto w-full">
       <div>
-      <div class="bg-white p-6 rounded-lg shadow-lg">
-    <h2 class="text-2xl font-bold mb-2 text-gray-800">RothC example simulation configuration</h2>
-    <p class="text-gray-700">Input the custom values in the fields below. Default values as placeholder text will be used if none provided.</p>
-  </div>
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+          <h2 class="text-2xl font-bold mb-2 text-gray-800">RothC example simulation configuration</h2>
+          <p class="text-gray-700">Input the custom values in the fields below. Default values as placeholder text will be used if none provided.</p>
+        </div>
+        <div class="flex flex-wrap mt-10">
 
+        <Rothctemplate
+        configParamtype="rainfall"
+        />
 
-          <div class="flex flex-wrap mt-10"> 
+        <Rothctemplate
+        configParamtype="openPanEvap"
+        />
 
-          <Rothctemplate 
-          configParamtype="rainfall"
-          />
+        <Rothctemplate
+        configParamtype="avgAirTemp"
+        />
 
-          <Rothctemplate 
-          configParamtype="openPanEvap"
-          />
+        <Rothctemplate
+        configParamtype="presCM"
+        />
 
-          <Rothctemplate 
-          configParamtype="avgAirTemp"
-          />
+        <Rothctemplate
+        configParamtype="soilCover"
+        />
 
-          <Rothctemplate 
-          configParamtype="presCM"
-          />
+        <Rothctemplate
+        configParamtype="initSoil"
+        />
 
-          <Rothctemplate 
-          configParamtype="soilCover"
-          />
-
-          <Rothctemplate
-          configParamtype="initSoil"
-          />
-
-          <Rothctemplate
-          configParamtype="soil"
-          />
-
-          </div>
+        <Rothctemplate
+        configParamtype="soil"
+        />
 
         </div>
+
+      </div>
     </div>
 </div>
 
@@ -51,7 +49,7 @@
 import Rothctemplate from "@/views/flint/Rothctemplate.vue"
 
 export default {
-  
+
   components: {
     Rothctemplate,
   },
