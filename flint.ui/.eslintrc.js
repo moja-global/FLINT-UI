635e2adc025b3b5c00d3ff819494340d68a370a4
aornugent <aornugent@gmail.com>
2021-08-01 13:49:12 +1000

    Add ESLint config

---
+++ flint.ui/.eslintrc.js
@@ -0,0 +1,14 @@
+// enable vue in eslint
+
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  plugins: ["vue"],
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended",
+  ],
+};
