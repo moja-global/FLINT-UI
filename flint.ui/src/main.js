967d3fa6f94bc570927aafa8bd72f8eeb5dc4f27
waridrox <mohdwarid4@gmail.com>
2021-08-08 04:30:56 +0530

    Fixed ESLint syntax

--- flint.ui/src/main.js
+++ flint.ui/src/main.js
@@ -6,37 +6,27 @@ import VueRouter from "vue-router";
 import App from "./App";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "@/assets/styles/tailwind.css";
+import "./index.css";
 
 import VueCompositionAPI from "@vue/composition-api";
 import store from "./store";
-const dataForge = require("data-forge");
-
-
 
 // router setup
 import routes from "./routes/routes";
-// import VueCircleSlider from "vue-circle-slider";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
-
-import LineChart from "@/components/Chart.vue";
-
 import VueApexCharts from "vue-apexcharts";
-Vue.use(VueApexCharts);
-
-Vue.component("apexchart", VueApexCharts);
-
 
 // configure router
 const router = new VueRouter({
   mode: "history",
-  routes,
+  routes
 });
 
 Vue.use(VueRouter);
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
 Vue.use(VueCompositionAPI);
-// Vue.use(VueCircleSlider);
 Vue.use(Toast);
 Vue.use(Vuex);
 
@@ -45,6 +35,6 @@ new Vue({
   el: "#app",
   store,
   router,
-  render: (h) => h(App),
   data: {},
+  render: h => h(App)
 });
