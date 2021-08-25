8739f81d3738ae8069908a08293c478105bee7f1
Shubham Karande <shubhamkarande13@gmail.com>
2021-08-26 05:24:05 +0530

    Add feature: Upload GCBM config & input files from UI!

--- flint.ui/src/components/FileUpload/FileUpload.vue
+++ flint.ui/src/components/FileUpload/FileUpload.vue
@@ -1,20 +1,51 @@
 <template>
   <div>
-    <p>Upload files here</p>
+    <button @click="add_title_to_formdata">
+      Add simulation title to formdata by clicking here
+    </button>
+    <br />
+    <br />
+    <p>Upload config files here</p>
     <vue-dropzone
-      id="dropzone"
-      ref="myVueDropzone"
-      :options="dropzoneOptions"
+      id="dropzoneConfig"
+      ref="myVueDropzoneConfig"
+      :options="dropzoneOptionsConfig"
       :use-custom-slot="true"
-      @vdropzone-success="uploadSuccess"
-      @vdropzone-error="uploadError"
       @vdropzone-removed-file="fileRemoved"
-      @vdropzone-sending="sendingEvent"
+      @vdropzone-file-added="fileAddedConfig"
+    ></vue-dropzone>
+
+    <br />
+    <p>Upload db files here</p>
+    <vue-dropzone
+      id="dropzoneDB"
+      ref="myVueDropzoneDB"
+      :options="dropzoneOptionsDB"
+      :use-custom-slot="true"
+      @vdropzone-removed-file="fileRemoved"
+      @vdropzone-file-added="fileAddedDB"
+    ></vue-dropzone>
+    <br />
+    <p>Upload input files here</p>
+    <vue-dropzone
+      id="dropzoneInput"
+      ref="myVueDropzoneInput"
+      :options="dropzoneOptionsInput"
+      :use-custom-slot="true"
+      @vdropzone-removed-file="fileRemoved"
+      @vdropzone-file-added="fileAddedInput"
     ></vue-dropzone>
     <br />
     <br />
     <button @click="listFiles">
       List uploaded files in console by clicking here
+    </button>
+    <br />
+    <br />
+    <br />
+
+    <button @click="add_title_to_formdata">
+      Log FormData in console by clicking here
     </button>
     <br />
     <br />
@@ -27,6 +58,7 @@
 
 <script>
 import vue2Dropzone from 'vue2-dropzone'
+import axios from 'axios'
 import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 export default {
   components: {
@@ -34,8 +66,33 @@ export default {
   },
   data: function () {
     return {
-      dropzoneOptions: {
-        url: 'https://httpbin.org/post',
+      formData: new FormData(),
+      dropzoneOptionsConfig: {
+        url: 'http://127.0.0.1/gcbm/upload',
+        autoQueue: true,
+        autoProcessQueue: false,
+        addRemoveLinks: true,
+        thumbnailWidth: 15,
+        maxFilesize: 1024,
+        uploadMultiple: true,
+        parallelUploads: 100,
+        maxFiles: 100,
+        createImageThumbnails: false
+      },
+      dropzoneOptionsDB: {
+        url: 'http://127.0.0.1/gcbm/upload',
+        autoQueue: true,
+        autoProcessQueue: false,
+        addRemoveLinks: true,
+        thumbnailWidth: 15,
+        maxFilesize: 1024,
+        uploadMultiple: true,
+        parallelUploads: 100,
+        maxFiles: 100,
+        createImageThumbnails: false
+      },
+      dropzoneOptionsInput: {
+        url: 'http://127.0.0.1/gcbm/upload',
         autoQueue: true,
         autoProcessQueue: false,
         addRemoveLinks: true,
@@ -50,39 +107,40 @@ export default {
   },
   methods: {
     listFiles: function () {
-      console.log(this.$refs.myVueDropzone.getAcceptedFiles())
+      console.log('list of config files')
+      console.log(this.$refs.myVueDropzoneConfig.getAcceptedFiles())
+      console.log('list of db files')
+      console.log(this.$refs.myVueDropzoneDB.getAcceptedFiles())
+      console.log('list of input files')
+      console.log(this.$refs.myVueDropzoneInput.getAcceptedFiles())
     },
-    uploadSuccess: function (file, response) {
-      console.log('File Successfully Uploaded with file name: ' + response.file)
-      this.fileName = response.file
-    },
-    uploadFailure: function (file, message) {
-      console.log('An Error Occurred')
-      console.log(file, message)
+    add_title_to_formdata: function () {
+      if (this.formData.entries().next().done === true) {
+        this.formData.append('title', this.$store.state.gcbm.title)
+      }
+      console.log([...this.formData])
     },
     triggerSend: function () {
-      this.$refs.myVueDropzone.processQueue()
+      axios
+        .post('http://localhost:8081/gcbm/upload', this.formData)
+        .then((response) => {
+          // this._vm.$toast.success(`${response}`, { timeout: 2000 })
+          console.log(response)
+          console.log(response.data)
+        })
+        .catch((error) => {
+          // this._vm.$toast.error(`${error}`, { timeout: 2000 })
+          console.log(error)
+        })
     },
-    sendingEvent: function (file, xhr, formData) {
-      console.log('file name')
-      if (file.name.includes('.db')) {
-        formData.append('db=@', file)
-        formData.append('input=@', file)
-      }
-      if (file.name.includes('.tiff')) {
-        formData.append('input=@', file)
-      }
-      if (file.name.includes('.json')) {
-        formData.append('input=@', file)
-      }
-      if (file.name.includes('.db')) {
-        formData.append('db=@', file)
-        formData.append('input=@', file)
-      }
-
-      //formData.append('_token', token)
-      console.log(formData)
-      formData.append('description', file)
+    fileAddedConfig(file) {
+      this.formData.append('config_files', file)
+    },
+    fileAddedDB(file) {
+      this.formData.append('db', file)
+    },
+    fileAddedInput(file) {
+      this.formData.append('input', file)
     },
     fileRemoved: function () {}
   }
