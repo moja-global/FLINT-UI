0997a9c2df4cfa88a909987b0696c9f50c2e9c57
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2021-10-28 03:00:55 +0530

    add vue-tour to flint ui
    Signed-off-by: Rajiv Ranjan Singh <rajivperfect007@gmail.com>

--- flint.ui/src/views/Landing.vue
+++ flint.ui/src/views/Landing.vue
@@ -1,58 +1,28 @@
 <template>
   <div>
-    <div
-      class="
-        relative
-        pt-16
-        pb-32
-        flex
-        content-center
-        items-center
-        justify-center
-        min-h-screen-75
-      "
-    >
+    <div class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
       <div
         class="absolute top-0 w-full h-full bg-center bg-cover"
-        style="
-          background-image: url('https://pbs.twimg.com/profile_banners/3438377559/1616958449/1500x500');
-        "
+        style="background-image: url('https://pbs.twimg.com/profile_banners/3438377559/1616958449/1500x500')"
       >
-        <span
-          id="blackOverlay"
-          class="w-full h-full absolute opacity-75 bg-black"
-        />
+        <span id="blackOverlay" class="w-full h-full absolute opacity-75 bg-black" />
       </div>
       <div class="container relative mx-auto">
         <div class="items-center flex flex-wrap">
           <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
             <div class="pr-12">
-              <h1 class="text-white font-semibold text-5xl">
-                About the FLINT UI.
-              </h1>
+              <h1 class="text-white font-semibold text-5xl">FLINT UI</h1>
               <p class="mt-4 text-lg text-white">
-                This project provides an intuitive way for new to explore some
-                preconfigured FLINT modules, including the Generic Budget Carbon
-                Model (GCBM), in order to better understand how the FLINT system
-                works. Our client is written as a Web application and can be
-                used in a local or remote environment.
+                This project provides an intuitive way for new to explore some preconfigured FLINT modules, including
+                the Generic Budget Carbon Model (GCBM), in order to better understand how the FLINT system works. Our
+                client is written as a Web application and can be used in a local or remote environment.
               </p>
             </div>
           </div>
         </div>
       </div>
       <div
-        class="
-          top-auto
-          bottom-0
-          left-0
-          right-0
-          w-full
-          absolute
-          pointer-events-none
-          overflow-hidden
-          h-70-px
-        "
+        class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
         style="transform: translateZ(0)"
       >
         <svg
@@ -64,101 +34,149 @@
           x="0"
           y="0"
         >
-          <polygon
-            class="text-blueGray-200 fill-current"
-            points="2560 0 2560 100 0 100"
-          />
+          <polygon class="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100" />
         </svg>
       </div>
     </div>
 
     <div class="container relative mx-auto">
-      <div class="items-center flex flex-wrap">
-        <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center mt-10">
-        <!--  <router-link to="/flint">
-            <button
-              class="
-                bg-white
-                text-gray-800
-                hover:bg-green-200
-                font-semibold
-                py-2
-                px-4
-                border border-gray-400
-                hover:border-green-600
-                rounded
-                shadow
-              "
-            >
-              Take me to FLINT UI
-            </button>
-          </router-link> -->
+      <div class="items-center flex flex-wrap justify-center">
+        <div>
+          <br />
+          <br />
+          <br />
+          <div class="p-12 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 gap-8">
+            <!-- <router-link to="/flint">
+              <button
+                class="
+                  bg-white
+                  text-lg text-gray-800
+                  hover:bg-green-200
+                  font-semibold
+                  py-10
+                  px-20
+                  border border-gray-400
+                  hover:border-green-600
+                  rounded
+                  shadow
+                "
+              >
+                Take me to FLINT UI
+              </button>
+            </router-link> -->
 
-          <router-link to="/flint/configurations/point">
-            <button
-              class="
-                bg-white
-                text-gray-800
-                hover:bg-green-200
-                font-semibold
-                py-2
-                px-4
-                border border-gray-400
-                hover:border-green-600
-                rounded
-                shadow
-              "
-            >
-              POINT
-            </button>
-          </router-link>
-          <br>
-          <br>
-          <router-link to="/flint/configurations/rothc">
-            <button
-              class="
-                bg-white
-                text-gray-800
-                hover:bg-green-200
-                font-semibold
-                py-2
-                px-4
-                border border-gray-400
-                hover:border-green-600
-                rounded
-                shadow
-              "
-            >
-              ROTHC
-            </button>
-          </router-link>
-          <br>
-          <br>
-          <router-link to="/gcbm/dashboard">
-            <button
-              class="
-                bg-white
-                text-gray-800
-                hover:bg-green-200
-                font-semibold
-                py-2
-                px-4
-                border border-gray-400
-                hover:border-green-600
-                rounded
-                shadow
-              "
-            >
-              GCBM
-            </button>
-          </router-link>
+            <router-link to="/flint/configurations/point">
+              <button
+                class="
+                  bg-white
+                  text-lg text-gray-800
+                  hover:bg-green-200
+                  font-semibold
+                  py-10
+                  px-20
+                  border border-gray-400
+                  hover:border-green-600
+                  rounded
+                  shadow
+                "
+              >
+                <div data-v-step="1">POINT</div>
+              </button>
+            </router-link>
+
+            <router-link to="/flint/configurations/rothc">
+              <button
+                class="
+                  bg-white
+                  text-lg text-gray-800
+                  hover:bg-green-200
+                  font-semibold
+                  py-10
+                  px-20
+                  border border-gray-400
+                  hover:border-green-600
+                  rounded
+                  shadow
+                "
+              >
+                <div data-v-step="2">ROTHC</div>
+              </button>
+            </router-link>
+
+            <router-link to="/gcbm/dashboard">
+              <button
+                class="
+                  bg-white
+                  text-lg text-gray-800
+                  hover:bg-green-200
+                  font-semibold
+                  py-10
+                  px-20
+                  border border-gray-400
+                  hover:border-green-600
+                  rounded
+                  shadow
+                "
+              >
+                <div data-v-step="3">GCBM</div>
+              </button>
+            </router-link>
+          </div>
+          <v-tour name="MyTour" :steps="steps" :options="myOptions"></v-tour>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   components: {}
+}
+</script>
+
+<script>
+export default {
+  name: 'MyTour',
+  data() {
+    return {
+      myOptions: {
+        useKeyboardNavigation: true,
+        labels: {
+          buttonSkip: 'Skip tour',
+          buttonPrevious: 'Previous',
+          buttonNext: 'Next',
+          buttonStop: 'Finish'
+        }
+      },
+      steps: [
+        {
+          target: '[data-v-step="1"]',
+          content:
+            'Point is a sample run of FLINT. To learn how to run the simulation using the FLINT UI dashboard follow the tour.',
+          params: {
+            placement: 'top'
+          }
+        },
+        {
+          target: '[data-v-step="2"]',
+          content: 'Click on this button to run RothC simulation.',
+          params: {
+            placement: 'top'
+          }
+        },
+        {
+          target: '[data-v-step="3"]',
+          content: 'Click on this button to run The Generic Carbon Budget Model (GCBM) simulation.',
+          params: {
+            placement: 'top'
+          }
+        }
+      ]
+    }
+  },
+  mounted: function () {
+    this.$tours['MyTour'].start()
+  }
 }
 </script>
--- flint.ui/src/views/flint/ConfigurationsPoint.vue
+++ flint.ui/src/views/flint/ConfigurationsPoint.vue
@@ -1,26 +1,12 @@
 <template>
-  <div
-    class="
-      relative
-      bg-gradient-to-r
-      from-green-400
-      to-blue-500
-      md:pt-32
-      pt-12
-      w-full
-    "
-    style="height: auto"
-  >
+  <div class="relative bg-gradient-to-r from-green-400 to-blue-500 md:pt-32 pt-12 w-full" style="height: auto">
     <div class="px-4 md:px-10 mx-auto w-full h-auto">
       <div>
         <div class="bg-white p-6 rounded-lg shadow-lg">
-          <h2 class="text-2xl font-bold mb-2 text-gray-800">
-            Point example simulation configuration
-          </h2>
+          <h2 class="text-2xl font-bold mb-2 text-gray-800">Point example simulation configuration</h2>
           <p>
             Was the simulation conducted at a specific place ? Click
-            <a> <span @click="isShow = !isShow">here</span> </a> to save
-            latitude and longitude co-ordinates on a
+            <a> <span @click="isShow = !isShow">here</span> </a> to save latitude and longitude co-ordinates on a
             <i class="fas fa-map mr-2 text-sm" />
           </p>
         </div>
@@ -31,12 +17,12 @@
 
         <div class="px-4 md:px-10 mx-auto w-full h-auto m-8">
           <div>
-            <div class="bg-white p-6 rounded-lg shadow-lg">
-              <h2 class="text-2xl font-bold mb-2 text-gray-800">
-                Start and End date of simulation
-              </h2>
-              <div>
-                <Datepicker />
+            <div data-v-step="1">
+              <div class="bg-white p-6 rounded-lg shadow-lg">
+                <h2 class="text-2xl font-bold mb-2 text-gray-800">Start and End date of simulation</h2>
+                <div>
+                  <Datepicker />
+                </div>
               </div>
             </div>
           </div>
@@ -45,47 +31,74 @@
         <div class="flex flex-wrap z-0">
           <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center">
             <div class="slider-component justify-center">
-              <div class="output">Pool 1: {{ pool1.value }}</div>
-              <vue-slider v-model="pool1.value" v-bind="pool1" />
+              <div data-v-step="2">
+                <div class="output">Pool 1: {{ pool1.value }}</div>
+                <vue-slider v-model="pool1.value" v-bind="pool1" />
+              </div>
             </div>
           </div>
 
           <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center">
             <div class="slider-component">
-              <div class="output">Pool 2: {{ pool2.value }}</div>
-              <vue-slider v-model="pool2.value" v-bind="pool2" />
+              <div data-v-step="3">
+                <div class="output">Pool 2: {{ pool2.value }}</div>
+                <vue-slider v-model="pool2.value" v-bind="pool2" />
+              </div>
             </div>
           </div>
 
           <div class="w-full lg:w-4/12 xl:w-4/12 px-4 content-center">
             <div class="slider-component">
-              <div class="output">Pool 3: {{ pool3.value }}</div>
-              <vue-slider v-model="pool3.value" v-bind="pool3" />
+              <div data-v-step="4">
+                <div class="output">Pool 3: {{ pool3.value }}</div>
+                <vue-slider v-model="pool3.value" v-bind="pool3" />
+              </div>
             </div>
           </div>
-          <button
-            class="
-              inline-block
-              align-middle
-              flex-initial
-              bg-white
-              hover:bg-black hover:text-white
-              text-gray-800
-              font-semibold
-              py-2
-              px-4
-              border border-gray-400
-              rounded
-              shadow
-            "
-            @click="Run()"
-          >
-            <i class="far fa-image" /> Run
-          </button>
+
+          <div class="items-center flex flex-wrap justify-center">
+            <div class="p-12 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 gap-8">
+              <button
+                class="
+                  bg-white
+                  text-lg text-gray-800
+                  hover:bg-black hover:text-white
+                  font-semibold
+                  py-4
+                  px-16
+                  border border-gray-400
+                  rounded
+                  shadow
+                "
+                @click="Run()"
+              >
+                <div data-v-step="5"><i class="far fa-play-circle" /> RUN</div>
+              </button>
+
+              <router-link to="/flint/point_output_table">
+                <button
+                  class="
+                    bg-white
+                    text-lg text-gray-800
+                    hover:bg-black hover:text-white
+                    font-semibold
+                    py-4
+                    px-16
+                    border border-gray-400
+                    rounded
+                    shadow
+                  "
+                >
+                  <div data-v-step="6"><i class="far fa-image" /> POINT OUTPUT TABLE</div>
+                </button>
+              </router-link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <Stepper />
+    <v-tour name="MyTour" :steps="steps" :options="myOptions"></v-tour>
   </div>
 </template>
 
@@ -121,8 +134,64 @@ export default {
         max: 400.0,
         interval: 5.0,
         value: 100.0
-      }
+      },
+      myOptions: {
+        useKeyboardNavigation: true,
+        labels: {
+          buttonSkip: 'Skip tour',
+          buttonPrevious: 'Previous',
+          buttonNext: 'Next',
+          buttonStop: 'Finish'
+        }
+      },
+      steps: [
+        {
+          target: '[data-v-step="1"]',
+          content: 'Set the Start and End date of simulation from the date picker.',
+          params: {
+            placement: 'left'
+          }
+        },
+        {
+          target: '[data-v-step="2"]',
+          content: 'Use the slider to set the Pool 1 value.',
+          params: {
+            placement: 'top'
+          }
+        },
+        {
+          target: '[data-v-step="3"]',
+          content: 'Use the slider to set the Pool 2 value.',
+          params: {
+            placement: 'top'
+          }
+        },
+        {
+          target: '[data-v-step="4"]',
+          content: 'Use the slider to set the Pool 3 value.',
+          params: {
+            placement: 'top'
+          }
+        },
+        {
+          target: '[data-v-step="5"]',
+          content: 'Click on the RUN button to start the simulation.',
+          params: {
+            placement: 'top'
+          }
+        },
+        {
+          target: '[data-v-step="6"]',
+          content: 'Click on this button to see the Point output.',
+          params: {
+            placement: 'top'
+          }
+        }
+      ]
     }
+  },
+  mounted: function () {
+    this.$tours['MyTour'].start()
   },
   created() {
     this.$root.$refs = this
@@ -145,14 +214,6 @@ export default {
 
     Run() {
       this.$root.$refs.finalPoolValues() //This does whatever the stepper does.
-
-      //If the setup is ok it will take the user to the Output else give an alert.
-      if (this.$store.state.point.flag === 1) {
-        this.$store.dispatch('parse_point_results')
-        window.open('http://127.0.0.1:8000/flint/point_output_table', '_self')
-      } else {
-        alert('Hey please check your setup')
-      }
     }
   }
 }
