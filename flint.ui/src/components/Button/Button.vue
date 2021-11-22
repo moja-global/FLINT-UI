15e74d0a0458a8f8d8cb22d83464e35a3a9eb106
Zaid Khan <zaidirfan.945@gmail.com>
2021-11-22 03:12:38 -0600

    Created Button component (#114)
    * design and developed button component
    
    * dynamic css vars on props
    
    * review fixes and button stories

---
+++ flint.ui/src/components/Button/Button.vue
@@ -0,0 +1,56 @@
+<template>
+  <button type="button" :style="btnStyles" @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+
+  props: {
+    isDisabled: {
+      type: Boolean,
+      default: false
+    },
+    btnSize: {
+      type: String,
+      default: '100px'
+    },
+    btnColor: {
+      type: String,
+      default: '#475447'
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('onClick')
+    }
+  },
+  computed: {
+    btnStyles() {
+      return {
+        '--color': this.btnColor,
+        '--width': this.btnSize
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  border: 1px solid #475447;
+  border-radius: 5px;
+  color: var(--btnColor);
+  width: var(--width);
+  font-family: 'DM Sans', sans-serif;
+  font-size: 15px;
+}
+button:hover {
+  color: #fff;
+  background-color: var(--color);
+  transition: all .5s;
+}
+</style>
