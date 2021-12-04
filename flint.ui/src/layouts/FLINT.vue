b7af6a8613378b30329b210183f253aa20fbc6b0
Himanshu <addyjeridiq@gmail.com>
2021-12-04 22:33:42 +0530

    UI: removed sidebar (#107)
    * removed sidebar
    
    * changes
    
    * removed sidebar
    
    Signed-off-by: himanshu007-creator <addyjeridiq@gmail.com>
    
    * changes
    
    Signed-off-by: himanshu007-creator <addyjeridiq@gmail.com>
    
    * typo

--- flint.ui/src/layouts/FLINT.vue
+++ flint.ui/src/layouts/FLINT.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <sidebar />
     <div class="relative md:ml-64 bg-blueGray-100">
       <div class="px-4 md:px-10 mx-auto w-full -m-24 h-screen">
         <router-view />
@@ -9,12 +8,7 @@
   </div>
 </template>
 <script>
-import Sidebar from '@/components/Sidebar/Sidebar.vue'
-
 export default {
-  name: 'FLINTLayout',
-  components: {
-    Sidebar
-  }
+  name: 'FLINTLayout'
 }
 </script>
