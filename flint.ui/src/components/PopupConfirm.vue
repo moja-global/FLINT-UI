59e0c74559d15e83859993a09079240b4773e38f
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2021-12-22 15:17:15 +0530

    feat: Add components
    Signed-off-by: Rajiv Ranjan Singh <rajivperfect007@gmail.com>

---
+++ flint.ui/src/components/PopupConfirm.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-popconfirm v-bind="$attrs" v-on="$listeners">
+    <slot></slot>
+  </a-popconfirm>
+</template>
+<script>
+import { Popconfirm } from 'ant-design-vue'
+export default {
+  name: 'BasePopconfirm', // optional
+  components: {
+    'a-popconfirm': Popconfirm
+  },
+  inheritAttrs: false
+}
+</script>
