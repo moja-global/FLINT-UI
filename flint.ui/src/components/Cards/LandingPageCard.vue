f6f968b9ac9fcfcf8d1b0660a20c451d433cf165
YashKandalkar <yashsk.yk@gmail.com>
2022-03-01 17:46:27 +0530

    feat: update landing page UI according to the new design
    Signed-off-by: YashKandalkar <yashsk.yk@gmail.com>

--- flint.ui/src/components/Cards/LandingPageCard.vue
+++ flint.ui/src/components/Cards/LandingPageCard.vue
@@ -1,39 +1,28 @@
 <template>
-  <div class="w-96">
+  <div class="w-96 mx-auto md:mx-0 flex flex-col justify-between">
     <div class="px-6 py-4">
-      <div class="py-2 font-bold text-2xl">
+      <div class="py-2 text-earth text-2xl">
         {{ title }}
       </div>
-      <p class="text-gray-700 text-base">
+      <p class="text-earth leading-7 text-base">
         {{ description }}
       </p>
     </div>
     <div class="px-6 pt-4 pb-2">
-      <router-link :to="link">
-        <div
-          class="
-            inline-block
-            border border-solid border-light-blue-500
-            bg-gray-200
-            rounded-md
-            px-4
-            py-2
-            text-sm
-            font-semibold
-            text-gray-700
-            mr-2
-            mb-2
-          "
-        >
-          Explore
-        </div>
-      </router-link>
+      <Button :btn-size="'auto'" class="flex" @click.native="onClick"
+        >Explore <span class="ml-1" style="margin-top: -2px"> →</span></Button
+      >
     </div>
   </div>
 </template>
 
 <script>
+import Button from '../Button/Button.vue'
+
 export default {
+  components: {
+    Button
+  },
   props: {
     title: { type: String, required: true },
     description: { type: String, required: true },
@@ -41,6 +30,11 @@ export default {
   },
   data: function () {
     return {}
+  },
+  methods: {
+    onClick: function () {
+      this.$router.push(this.link)
+    }
   }
 }
 </script>
--- flint.ui/src/views/Landing.vue
+++ flint.ui/src/views/Landing.vue
@@ -2,9 +2,7 @@
   <div>
     <LandingPageNavbar />
     <div class="flex flex-col flex-wrap lg:flex-row">
-      <div
-        class="relative pt-16 pb-32 flex content-center items-center justify-center w-full min-h-screen-75 lg:w-5/12"
-      >
+      <div class="relative py-32 lg:pt-28 flex content-center justify-center w-full min-h-screen-75 lg:w-5/12">
         <div
           class="absolute top-0 w-full h-full bg-center bg-cover"
           style="background-image: url('https://pbs.twimg.com/profile_banners/3438377559/1616958449/1500x500')"
@@ -12,17 +10,17 @@
           <span id="blackOverlay" class="w-full h-full absolute opacity-75 bg-black" />
         </div>
         <div class="container relative mx-auto">
-          <div class="items-center flex flex-wrap">
-            <div class="w-1/4 lg:w-1/2 px-4 ml-auto mr-auto text-center">
-              <div class="lg:pr-12">
-                <h1 class="text-white font-semibold text-5xl">FLINT UI</h1>
+          <div class="items-center lg:items-end flex flex-wrap">
+            <div class="lg:w-1/2 px-4 mx-auto lg:ml-auto lg:mr-2 text-center">
+              <div class="">
+                <h1 class="text-white whitespace-nowrap text-6xl">Flint UI</h1>
               </div>
             </div>
           </div>
         </div>
       </div>
       <div class="bg-earth w-full lg:w-7/12 text-white flex items-center">
-        <p class="text-lg text-white px-36 text-xl py-36">
+        <p class="text-white px-8 sm:px-16 text-base md:text-xl leading-7 md:leading-9 py-14 md:py-20">
           FLINT-UI provides an intuitive way to explore preconfigured FLINT modules, including the Generic Budget Carbon
           Model (GCBM). Through the FLINT-UI, you can provide relevant datasets from custom or public data providers,
           dispatch the job to the FLINT engine for processing, through our user-interface on a local or remote
@@ -32,7 +30,7 @@
     </div>
 
     <div>
-      <div class="items-start md:flex flex-wrap w-full md:justify-center space-x-10 my-10 pb-20">
+      <div class="md:flex flex-wrap w-full md:justify-center my-10 pb-20">
         <LandingPageCard
           title="Point"
           description="Point is a basic model int the FLINT that allows a person to enter three pool values, date, location. This is a good model to practice and a good starting point."
