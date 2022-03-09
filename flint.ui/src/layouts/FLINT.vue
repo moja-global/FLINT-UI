0e9fde4e1627e3a621a26e160230843fd7d94a30
yash <35102959+YashKandalkar@users.noreply.github.com>
2022-03-09 22:26:35 +0530

    CHORE: Refactor the configurations point UI (#164)
    Signed-off-by: YashKandalkar <yashsk.yk@gmail.com>

--- flint.ui/src/layouts/FLINT.vue
+++ flint.ui/src/layouts/FLINT.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="relative md:ml-64 bg-blueGray-100">
-      <div class="px-4 md:px-10 mx-auto w-full -m-24 h-screen">
+    <div class="relative">
+      <div class="px-4 md:px-10 mx-auto w-full">
         <router-view />
       </div>
     </div>
