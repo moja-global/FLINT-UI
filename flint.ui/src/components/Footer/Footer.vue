3289de711256255ffcabe3aad017a2cbecebc71f
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2022-03-11 04:28:46 +0530

    update to ant design

--- flint.ui/src/components/Footer/Footer.vue
+++ flint.ui/src/components/Footer/Footer.vue
@@ -1,115 +1,99 @@
 <template>
-  <div class="bg-gray-400 pb-20">
-    <div class="flex justify-center pt-16 pb-5 tablet:flex-col tablet:gap-5 tablet:pl-5">
-      <div class="flex justify-center items-start gap-10 tablet:justify-start">
-        <div>
+  <Footer style="padding-top: 3.5rem; padding-bottom: 5rem; background-color: #a6adb4">
+    <a-row style="justify-content: center">
+      <a-col :xm="24" :sm="24" :md="15" :lg="15" :xl="15"
+        ><a-space
+          direction="horizontal"
+          size="middle"
+          style="display: flex; align-items: center; justify-content: center"
+        >
           <img
-            class="w-36"
+            style="width: 9rem"
             src="https://i0.wp.com/moja.global/wp-content/uploads/2021/03/our-contributors-graphic-300x300.png"
           />
-        </div>
-        <div class="grid grid-rows-3 text-white items-start">
-          <div class="text-xl font-medium">Get In Touch</div>
-          <div class="flex items-center gap-2.5">
-            <i class="fas fa-envelope" />
-            <h1>info@moja.global</h1>
-          </div>
-          <div class="flex gap-2">
-            <a
-              class="rounded-full h-12 w-12 bg-white flex justify-center items-center"
-              href="https://twitter.com/mojaglobal"
-              target="_blank"
-            >
-              <i class="fab fa-twitter text-2xl text-gray-400"></i>
-            </a>
-            <a
-              class="rounded-full h-12 w-12 bg-white flex justify-center items-center hover:no-underline"
-              href="https://www.linkedin.com/company/moja-global/"
-              target="_blank"
-            >
-              <i class="fab fa-linkedin text-2xl text-gray-400"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="flex justify-end items-start gap-20 text-base text-white font-normal tablet:justify-start">
-        <div class="grid gap-5">
-          <h1 class="text-xl font-medium">Site Links</h1>
-          <h1>
-            <a href="https://moja.global/about/" target="_blank" style="text-decoration: none; color: inherit">About</a>
-          </h1>
-          <h1>
-            <a href="https://moja.global/what-we-do/" target="_blank" style="text-decoration: none; color: inherit"
-              >What We Do</a
-            >
-          </h1>
-          <h1>
-            <a href="https://moja.global/flint/" target="_blank" style="text-decoration: none; color: inherit">FLINT</a>
-          </h1>
-          <h1>
-            <a href="https://moja.global/get-involved/" target="_blank" style="text-decoration: none; color: inherit"
-              >Get Involved</a
-            >
-          </h1>
-          <h1>
-            <a href="https://moja.global/resources/" target="_blank" style="text-decoration: none; color: inherit"
-              >Resources</a
-            >
-          </h1>
-          <h1>
-            <a href="https://moja.global/news/" target="_blank" style="text-decoration: none; color: inherit">News</a>
-          </h1>
-        </div>
-        <div class="grid grid-cols-1 w-1/3 gap-5">
-          <h1 class="text-xl font-medium">News</h1>
-          <h1>
-            <a
-              href="https://moja.global/moja-global-wins-azure-cloud-grant/"
-              target="_blank"
-              style="text-decoration: none; color: inherit"
-              >Moja Global Wins Azure Cloud Grant</a
-            >
-          </h1>
-          <h1>
-            <a
-              href="https://moja.global/generic-plan-in-sprints-to-implement-of-flint-based-mrv-system/"
-              target="_blank"
-              style="text-decoration: none; color: inherit"
-              >Generic Plan in Sprinrs To Implement of FLINT-Based MRV System</a
-            >
-          </h1>
-          <h1>
-            <a
-              href="https://moja.global/challenges-for-open-source-projects-when-working-with-public-international-organisations/"
-              target="_blank"
-              style="text-decoration: none; color: inherit"
-              >Challengers for Open Source Projects When Working With Public Interntional Organisations</a
-            >
-          </h1>
-          <button class="border-solid border-2 border-white py-2 text-xs w-2/3">
-            <a
-              style="text-decoration: none; color: inherit"
-              href="https://global.us13.list-manage.com/subscribe?u=d8a187fa0dfae0bafe4bc69cc&id=cbfdc2d476"
-              target="_blank"
-            >
-              Subscribe to our newsletter
-            </a>
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="flex justify-center items-center text-gray-500">
-      Copyright 2021 |&nbsp;<a
-        style="text-decoration: none; color: inherit; color: #74e8a3"
-        href="https://staging2.kylec24.sg-host.com/cookies/"
-      >
-        Cookies </a
-      >&nbsp;|&nbsp;
-      <a style="text-decoration: none; color: #74e8a3" href="https://staging2.kylec24.sg-host.com/privacy-policy/"
-        >Privacy Policy</a
-      >
-    </div>
-  </div>
+          <a-space direction="vertical" size="middle" style="color: white">
+            <a-typography-text style="color: white; font-size: 1.2rem">Get In Touch</a-typography-text>
+            <a-space direction="horizontal" size="small">
+              <i class="fas fa-envelope" />
+              <a-typography-title style="color: white">info@moja.global</a-typography-title>
+            </a-space>
+            <a-space direction="horizontal" size="small">
+              <a-button
+                type="text"
+                shape="circle"
+                href="https://twitter.com/mojaglobal"
+                style="
+                  background-color: white;
+                  width: 3rem;
+                  height: 3rem;
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                "
+              >
+                <i class="fab fa-twitter fa-2x" style="color: #a6adb4"></i>
+              </a-button>
+              <a-button
+                type="text"
+                shape="circle"
+                href="https://www.linkedin.com/company/moja-global/"
+                style="
+                  background-color: white;
+                  width: 3rem;
+                  height: 3rem;
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                "
+              >
+                <i class="fab fa-linkedin fa-2x" style="color: #a6adb4"></i> </a-button></a-space
+          ></a-space> </a-space
+      ></a-col>
+      <a-col :xm="10" :sm="10" :md="0" :lg="0" :xl="0"></a-col>
+      <a-col :xm="14" :sm="14" :md="9" :lg="9" :xl="9">
+        <a-space direction="vertical" size="middle" style="display: flex">
+          <a-typography-title style="color: white; font-size: 1.2rem">Site Links</a-typography-title>
+          <a-typography-link href="https://moja.global/about/" target="_blank" style="color: white">
+            About
+          </a-typography-link>
+          <a-typography-link href="https://moja.global/what-we-do/" target="_blank" style="color: white">
+            What We Do
+          </a-typography-link>
+          <a-typography-link href="https://moja.global/flint/" target="_blank" style="color: white">
+            FLINT
+          </a-typography-link>
+          <a-typography-link href="https://moja.global/get-involved/" target="_blank" style="color: white">
+            Get Involved
+          </a-typography-link>
+          <a-typography-link href="https://moja.global/resources/" target="_blank" style="color: white">
+            Resources
+          </a-typography-link>
+          <a-typography-link href="https://moja.global/news/" target="_blank" style="color: white">
+            News
+          </a-typography-link>
+        </a-space>
+      </a-col>
+    </a-row>
+    <a-space
+      direction="horizontal"
+      size="small"
+      style="display: flex; align-items: center; justify-content: center; margin-top: 1rem; color: #7a7a7a"
+    >
+      Copyright 2021 |
+      <a-typography-link
+        href="https://staging2.kylec24.sg-host.com/privacy-policy/"
+        target="_blank"
+        style="color: #74dfac"
+        >Cookies </a-typography-link
+      >|
+      <a-typography-link
+        href="https://staging2.kylec24.sg-host.com/privacy-policy/"
+        target="_blank"
+        style="color: #74dfac"
+        >Privacy Policies
+      </a-typography-link>
+    </a-space>
+  </Footer>
 </template>
 
 <script>
