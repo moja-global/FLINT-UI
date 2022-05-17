c0815b1777d4b4d15c127e0b4130985640bacc69
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-17 14:19:16 +0100

    refactor: changed stepper static layout
    Signed-off-by: Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>

--- flint.ui/src/views/gcbm/GCBMDashboard.vue
+++ flint.ui/src/views/gcbm/GCBMDashboard.vue
@@ -8,62 +8,60 @@
           <p class="text-gray-700">Follow the steps below to simulate GCBM runs.</p>
         </div>
 
-        <div class="flex flex-col mt-4">
-          <StepperStatic />
+        <StepperStatic />
 
-          <div class="w-full mt-6">
-            <div class="w-4/12 px-4 mx-auto content-center">
-              <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
-                <div class="flex-auto p-4">
-                  <div class="relative w-full max-w-full flex-grow flex-1">
-                    <span class="font-semibold text-xl text-blueGray-700"> Create a new Simulation </span>
+        <div class="w-full mt-6">
+          <div class="w-4/12 px-4 mx-auto content-center">
+            <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+              <div class="flex-auto p-4">
+                <div class="relative w-full max-w-full flex-grow flex-1">
+                  <span class="font-semibold text-xl text-blueGray-700"> Create a new Simulation </span>
 
-                    <input
-                      v-model="simulation_title"
-                      class="
-                        w-full
-                        mt-4
-                        h-10
-                        px-3
-                        mb-2
-                        text-base text-gray-700
-                        placeholder-gray-600
-                        border
-                        rounded-lg
-                        focus:shadow-outline
-                      "
-                      type="text"
-                      placeholder="Enter title for simulation"
-                    />
+                  <input
+                    v-model="simulation_title"
+                    class="
+                      w-full
+                      mt-4
+                      h-10
+                      px-3
+                      mb-2
+                      text-base text-gray-700
+                      placeholder-gray-600
+                      border
+                      rounded-lg
+                      focus:shadow-outline
+                    "
+                    type="text"
+                    placeholder="Enter title for simulation"
+                  />
 
-                    <button
-                      class="
-                        w-full
-                        mt-4
-                        block
-                        align-middle
-                        flex-initial
-                        bg-white
-                        hover:bg-earth hover:text-white
-                        text-gray-800
-                        font-semibold
-                        py-2
-                        px-4
-                        border border-gray-400
-                        rounded
-                        shadow
-                      "
-                      :disabled="isTitle()"
-                      :class="{ 'opacity-25 cursor-not-allowed': isTitle() }"
-                      @click="sendToAPI"
-                    >
-                      <i class="fas fa-plus" /> Create run
-                    </button>
-                  </div>
-                  <p class="text-sm text-blueGray-400 mt-4">
-                    <span class="whitespace-nowrap">Creates a new simulation run</span>
-                  </p>
+                  <button
+                    class="
+                      w-full
+                      mt-4
+                      block
+                      align-middle
+                      flex-initial
+                      bg-white
+                      hover:bg-earth hover:text-white
+                      text-gray-800
+                      font-semibold
+                      py-2
+                      px-4
+                      border border-gray-400
+                      rounded
+                      shadow
+                    "
+                    :disabled="isTitle()"
+                    :class="{ 'opacity-25 cursor-not-allowed': isTitle() }"
+                    @click="sendToAPI"
+                  >
+                    <i class="fas fa-plus" /> Create run
+                  </button>
                 </div>
+                <p class="text-sm text-blueGray-400 mt-4">
+                  <span class="whitespace-nowrap">Creates a new simulation run</span>
+                </p>
               </div>
             </div>
           </div>
