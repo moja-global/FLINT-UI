f4173a859a4a550164ac0a85c74df4c09a378538
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-17 18:48:48 +0100

    refactor: made stepper GCBM responsive on mobile
    Signed-off-by: Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>

--- flint.ui/src/components/Stepper/StepperGCBM.vue
+++ flint.ui/src/components/Stepper/StepperGCBM.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="sticky bottom-0 mt-0 z-50">
-    <md-steppers md-sync-route md-dynamic-height>
+  <div class="sticky bottom-0">
+    <md-steppers md-sync-route md-dynamic-height md-alternative class="large-screen">
       <md-step id="first" to="/gcbm/dashboard" md-label="New Simulation" />
 
       <md-step id="second" to="/gcbm/upload" md-label="Upload dataset" />
 
-      <md-step id="third" to="/gcbm/run" md-label="Run | Check status | Download" />
+      <md-step id="third" to="/gcbm/run" md-label="Run | Status | Download" />
+    </md-steppers>
+
+    <md-steppers md-sync-route md-dynamic-height md-vertical class="small-screen">
+      <md-step id="first" to="/gcbm/dashboard" md-label="New Simulation" />
+
+      <md-step id="second" to="/gcbm/upload" md-label="Upload dataset" />
+
+      <md-step id="third" to="/gcbm/run" md-label="Run | Check Status | Download" />
     </md-steppers>
   </div>
 </template>
@@ -15,3 +23,21 @@ export default {
   methods: {}
 }
 </script>
+
+<style scoped>
+.small-screen {
+  display: block;
+}
+.large-screen {
+  display: none;
+}
+
+@media screen and (min-width: 510px) {
+  .small-screen {
+    display: none;
+  }
+  .large-screen {
+    display: block;
+  }
+}
+</style>
