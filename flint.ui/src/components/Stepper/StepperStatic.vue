53bff233e0cad9ec146ff5ae56e575bca63345a4
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-17 18:50:15 +0100

    refactor: made stepper static responsive on all
    Signed-off-by: Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>

--- flint.ui/src/components/Stepper/StepperStatic.vue
+++ flint.ui/src/components/Stepper/StepperStatic.vue
@@ -1,64 +1,121 @@
 <template>
-  <div class="my-5">
-    <div class="flex">
-      <div class="w-1/3 text-center px-6">
-        <div class="bg-gray-300 rounded-lg flex items-center justify-center border border-gray-200">
-          <div class="w-1/3 bg-transparent h-20 flex items-center justify-center icon-step">
+  <div class="mt-14 flex items-center justify-center">
+    <a-row class="p-4 w-full" :gutter="[24, 24]">
+      <a-col :xs="{ span: 24 }" :md="{ span: 12 }" :xl="{ span: 6 }">
+        <a-row class="cards" :gutter="[6, 6]">
+          <a-col
+            :xs="{ span: 24 }"
+            :md="{ span: 20 }"
+            :xl="{ span: 20 }"
+            class="bg-gray-300 border border-gray-200 card-content"
+          >
             <i class="fas fa-plus fa-lg" />
-          </div>
-          <div class="w-2/3 bg-gray-200 h-24 flex flex-col items-center justify-center px-1 rounded-r-lg body-step">
-            <h2 class="font-bold text-sm">Create a new simulation</h2>
-            <p class="text-xs text-gray-600">Click the new button below</p>
-          </div>
-        </div>
-      </div>
-      <div class="flex-1 flex items-center justify-center">
-        <i class="fas fa-arrow-right fa-2x" />
-      </div>
-      <div class="w-1/3 text-center px-6">
-        <div class="bg-gray-300 rounded-lg flex items-center justify-center border border-gray-200">
-          <div class="w-1/3 bg-transparent h-20 flex items-center justify-center icon-step">
-            <i class="far fa-file fa-lg" />
-          </div>
-          <div class="w-2/3 bg-gray-200 h-24 flex flex-col items-center justify-center px-1 rounded-r-lg body-step">
-            <h2 class="font-bold text-sm">Upload file</h2>
-            <p class="text-xs text-gray-600">Dataset file for GCBM</p>
-          </div>
-        </div>
-      </div>
-      <div class="flex-1 flex items-center justify-center">
-        <i class="fas fa-arrow-right fa-2x" />
-      </div>
-      <div class="w-1/3 text-center px-6">
-        <div class="bg-gray-300 rounded-lg flex items-center justify-center border border-gray-200">
-          <div class="w-1/3 bg-transparent h-20 flex items-center justify-center icon-step">
-            <i class="fas fa-cogs fa-lg" />
-          </div>
-          <div class="w-2/3 bg-gray-200 h-24 flex flex-col items-center justify-center px-1 rounded-r-lg body-step">
-            <h2 class="font-bold text-sm">Configure</h2>
-            <p class="text-xs text-gray-600">Change configuration parameters</p>
-          </div>
-        </div>
-      </div>
+            <div class="bg-gray-200 rounded-r-lg body-step">
+              <h2 class="font-bold text-sm">Create a new simulation</h2>
+              <p class="text-xs text-gray-600">Click the new button below</p>
+            </div>
+          </a-col>
+          <a-col class="next-icons" :xs="{ span: 24 }" :md="{ span: 4 }" :xl="{ span: 4 }">
+            <i class="fas fa-arrow-right fa-2x" />
+          </a-col>
+        </a-row>
+      </a-col>
 
-      <div class="flex-1 flex items-center justify-center">
-        <i class="fas fa-arrow-right fa-2x" />
-      </div>
-      <div class="w-1/3 text-center px-6">
-        <div class="bg-gray-300 rounded-lg flex items-center justify-center border border-gray-200">
-          <div class="w-1/3 bg-transparent h-20 flex items-center justify-center icon-step">
+      <a-col :xs="{ span: 24 }" :md="{ span: 12 }" :xl="{ span: 6 }">
+        <a-row class="cards" :gutter="[6, 6]">
+          <a-col
+            :xs="{ span: 24 }"
+            :md="{ span: 20 }"
+            :xl="{ span: 20 }"
+            class="bg-gray-300 border border-gray-200 card-content"
+          >
+            <i class="fas fa-file fa-lg" />
+            <div class="bg-gray-200 rounded-r-lg body-step">
+              <h2 class="font-bold text-sm">Upload file</h2>
+              <p class="text-xs text-gray-600">Dataset file for GCBM</p>
+            </div>
+          </a-col>
+          <a-col class="next-icons" :xs="{ span: 24 }" :md="{ span: 4 }" :xl="{ span: 4 }">
+            <i class="fas fa-arrow-right fa-2x" />
+          </a-col>
+        </a-row>
+      </a-col>
+
+      <a-col :xs="{ span: 24 }" :md="{ span: 12 }" :xl="{ span: 6 }">
+        <a-row class="cards" :gutter="[6, 6]">
+          <a-col
+            :xs="{ span: 24 }"
+            :md="{ span: 20 }"
+            :xl="{ span: 20 }"
+            class="bg-gray-300 border border-gray-200 card-content"
+          >
+            <i class="fas fa-cogs fa-lg" />
+            <div class="bg-gray-200 rounded-r-lg body-step">
+              <h2 class="font-bold text-sm">Configure</h2>
+              <p class="text-xs text-gray-600">Change configuration parameters</p>
+            </div>
+          </a-col>
+          <a-col class="next-icons" :xs="{ span: 24 }" :md="{ span: 4 }" :xl="{ span: 4 }">
+            <i class="fas fa-arrow-right fa-2x" />
+          </a-col>
+        </a-row>
+      </a-col>
+
+      <a-col :xs="{ span: 24 }" :md="{ span: 12 }" :xl="{ span: 6 }">
+        <a-row class="cards" :gutter="[6, 6]">
+          <a-col
+            :xs="{ span: 24 }"
+            :md="{ span: 20 }"
+            :xl="{ span: 20 }"
+            class="bg-gray-300 border border-gray-200 card-content"
+          >
             <i class="fas fa-check fa-lg" />
-          </div>
-          <div class="w-2/3 bg-gray-200 h-24 flex flex-col items-center justify-center px-1 rounded-r-lg body-step">
-            <h2 class="font-bold text-sm">Run</h2>
-            <p class="text-xs text-gray-600">Run the simulation with new configs</p>
-          </div>
-        </div>
-      </div>
-    </div>
+            <div class="bg-gray-200 rounded-r-lg body-step">
+              <h2 class="font-bold text-sm">Run</h2>
+              <p class="text-xs text-gray-600">Run the simulation with new configs</p>
+            </div>
+          </a-col>
+        </a-row>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
-<script>
-export default {}
-</script>
+<style scoped>
+.cards {
+  display: flex;
+  align-items: center;
+}
+.card-content {
+  display: flex;
+  align-items: center;
+  max-width: 250px;
+}
+.card-content > i {
+  margin: 10px;
+}
+.card-content > div {
+  margin: 10px;
+  padding: 5px 10px;
+  height: 100px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.next-icons {
+  margin-left: 10px;
+}
+@media screen and (max-width: 768px) {
+  .cards {
+    flex-direction: column;
+  }
+  .next-icons {
+    display: flex;
+    justify-content: center;
+    transform: rotate(90deg);
+    margin-left: 0px;
+    margin-top: 20px;
+  }
+}
+</style>
