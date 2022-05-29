256deb3fa63a1ceacda44b248a52ece47240f16e
Sanjay Singh Rajpoot <67458417+SanjaySinghRajpoot@users.noreply.github.com>
2022-05-29 12:51:13 +0530

    Updated GCBM URL's

--- flint.ui/src/views/gcbm/GCBMRun.vue
+++ flint.ui/src/views/gcbm/GCBMRun.vue
@@ -117,7 +117,7 @@ export default {
       console.log([...bodyFormData])
 
       axios
-        .post('process.env.VUE_APP_REST_API_GCBM/gcbm/dynamic', bodyFormData)
+        .post(`${process.env.VUE_APP_REST_API_GCBM/gcbm/dynamic}`, bodyFormData)
         .then((response) => {
           this.$toast.success(`${response.data.status}`, { timeout: 5000 })
           console.log(response)
@@ -135,7 +135,7 @@ export default {
       console.log([...bodyFormData])
 
       axios
-        .post('process.env.VUE_APP_REST_API_GCBM/gcbm/status', bodyFormData)
+        .post(`${process.env.VUE_APP_REST_API_GCBM}/gcbm/status`, bodyFormData)
         .then((response) => {
           this.$toast.info(`${response.data.finished}`, { timeout: 5000 })
           this.$store.commit('setSimulationProgressState', response.data.finished)
@@ -155,7 +155,7 @@ export default {
       console.log([...bodyFormData])
 
       axios
-        .post('process.env.VUE_APP_REST_API_GCBM/gcbm/download', bodyFormData, {
+        .post(`${process.env.VUE_APP_REST_API_GCBM}/gcbm/download`, bodyFormData, {
           responseType: 'arraybuffer'
         })
         .then((response) => {
