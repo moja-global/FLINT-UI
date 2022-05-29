9fad61a2257f08aaa7410f6a7d38981829c5881d
YashKandalkar <yashsk.yk@gmail.com>
2022-05-29 16:47:19 +0530

    feat: fix RothC UI

---
+++ flint.ui/src/components/ConfigurationsRothC/RothCPresCM.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-full content-center rounded-md">
+      <h1 class="flex-auto text-base text-earth font-normal">presCM - data_orig</h1>
+      <textarea
+        v-model="newconfig_presCM_data_orig"
+        class="form-textarea rounded-md mt-1 block w-full"
+        rows="5"
+        placeholder=""
+      >
+      </textarea>
+    </div>
+
+    <div class="w-full content-center rounded-md">
+      <h1 class="flex-auto text-base text-earth font-normal mt-5">presCM - data_month_avg</h1>
+      <textarea
+        v-model="newconfig_presCM_data_month_avg"
+        class="form-textarea rounded-md mt-1 block w-full"
+        rows="5"
+        placeholder=""
+      >
+      </textarea>
+    </div>
+
+    <div class="w-full content-center rounded-md">
+      <h1 class="flex-auto text-base text-earth font-normal mt-5">presCM - data_lastyearcopy</h1>
+      <textarea
+        v-model="newconfig_presCM_data_lastyearcopy"
+        class="form-textarea rounded-md mt-1 block w-full"
+        rows="5"
+        placeholder=""
+      >
+      </textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+import formatData from '@/mixins/formatData'
+
+export default {
+  mixins: [formatData],
+  computed: {
+    newconfig_presCM_data_orig: {
+      get() {
+        return this.formatArray(this.$store.state.rothc.config.Variables[11].presCM.transform.data_orig)
+      },
+      set(newValue) {
+        this.$store.commit('setNewConfig_presCM_data_orig', '$#[ ' + newValue + ' ]$#')
+      }
+    },
+
+    newconfig_presCM_data_month_avg: {
+      get() {
+        return this.formatArray(this.$store.state.rothc.config.Variables[11].presCM.transform.data_month_avg)
+      },
+      set(newValue) {
+        this.$store.commit('setNewConfig_presCM_data_month_avg', '$#[ ' + newValue + ' ]$#')
+      }
+    },
+
+    newconfig_presCM_data_lastyearcopy: {
+      get() {
+        return this.formatArray(this.$store.state.rothc.config.Variables[11].presCM.transform.data_lastyearcopy)
+      },
+      set(newValue) {
+        this.$store.commit('setNewConfig_presCM_data_lastyearcopy', '$#[ ' + newValue + ' ]$#')
+      }
+    }
+  }
+}
+</script>
