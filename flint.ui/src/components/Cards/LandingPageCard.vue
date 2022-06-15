b60ee8a11b94d7aab6fc20d25e7b6eaab1ccad09
Janvi <janvithakkar.583@gmail.com>
2022-06-15 23:02:47 +0530

    feat: added cards to Point, RothC and GCBM sections of landing page

--- flint.ui/src/components/Cards/LandingPageCard.vue
+++ flint.ui/src/components/Cards/LandingPageCard.vue
@@ -1,15 +1,21 @@
 <template>
-  <a-col :span="24" :md="{ span: 12 }" :xl="{ span: 8 }" class="card">
-    <div>
-      <h2 class="text-earth text-2xl mb-2">
-        {{ title }}
-      </h2>
-      <p class="text-earth text-base leading-7">
-        {{ description }}
-      </p>
-      <Button class="mt-4" :btn-size="'auto'" @click="onClick">Explore <span> →</span></Button>
-    </div>
-  </a-col>
+      <a-col type="flex" align="center" :span="24" :md="{ span: 11 }" :xl="{ span: 7 }" class="card bg-white rounded shadow-lg">
+            <a-col>
+                <h2 class="text-earth text-2xl mb-2">
+                    {{ title }}
+                </h2>
+                <p class="text-earth text-base leading-7 justify-text">
+                    {{ description }}
+                </p>
+            </a-col>
+
+            <a-row type="flex" justify="center">
+                <a-space>
+                    <Button class="mt-4 ml-2 mr-2" :btn-size="'auto'" @click="onClick">Explore <span> →</span></Button>
+                    <Button class="mt-4" :btn-size="'auto'" @click="goDocs">Docs <span> →</span></Button>
+                </a-space>
+            </a-row>
+      </a-col>
 </template>
 
 <script>
@@ -24,26 +30,46 @@ export default {
   props: {
     title: { type: String, required: true },
     description: { type: String, required: true },
-    link: { type: String, required: true }
-  },
-  // data: function () {
-  //   return {}
-  // },
+    link: { type: String, required: true },
+    docLink: { type: String, required: true }
+        },
+
   setup (props) {
     const link = ref(props.link)
+    const docLink = ref(props.docLink)
     const router = useRouter()
     
- function onClick () {
-      router.push(link.value)
-    }
-    return { 
-      onClick
-    }
-  } 
+      function onClick () {
+        router.push(link.value)
+      }
+
+      function goDocs() {
+        return window.open(docLink.value)
+      }
+      return { 
+        onClick,goDocs
+      }
+  }
 }
 </script>
 
 <style scoped>
+    .card {
+        padding: 16px 8px;
+        transition:transform 0.3s;
+        display:flex;
+        flex-direction:column;
+        justify-content:space-between;
+    }
+
+    .card:hover {
+        transform:scale(1.03)
+    }
+
+    .justify-text{
+        text-align:justify
+    }
+
 @media screen and (min-width: 1400px) {
   .card {
     max-width: 350px;
@@ -58,5 +84,6 @@ export default {
     margin-left: auto;
     margin-right: auto;
   }
+
 }
 </style>
