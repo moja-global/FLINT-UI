ab190a2dee5463d1d95c50ebbd9419736b4f4195
YashKandalkar <yashsk.yk@gmail.com>
2022-06-17 15:41:38 +0530

    feat: add Sidebar, LocalDomain in GCBM. WIP.

--- flint.ui/src/routes/routes.js
+++ flint.ui/src/routes/routes.js
@@ -71,6 +71,11 @@ const routes = [
         ]
       },
       {
+        name: 'gcbmconfigurations',
+        path: '/gcbm/configurations',
+        component: GCBMDashboard
+      },
+      {
         name: 'gcbmupload',
         path: '/gcbm/upload',
         component: GCBMUpload
