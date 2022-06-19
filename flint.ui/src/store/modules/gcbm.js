4be1caae0c23ede314bf6c708ac977c25437750f
Amit <51860725+CrystalSage@users.noreply.github.com>
2022-06-19 21:03:15 +0530

    refactor: Migrate from Toasts to Notifications (#316)
    * refactor: Replace toasts with notifications
    
    Replaces vue-toastification toasts with Ant Design
    notifications, allowing us to get rid of the dependency.
    
    Signed-off-by: Amit <amitkulkarni2028@gmail.com>
    
    * build: Remove toast dependency, fix lint errors
    
    Signed-off-by: Amit <amitkulkarni2028@gmail.com>

--- flint.ui/src/store/modules/gcbm.js
+++ flint.ui/src/store/modules/gcbm.js
@@ -1,8 +1,11 @@
 import { notification } from 'ant-design-vue'
 import { notification } from 'ant-design-vue'
 import axios from 'axios'
+<<<<<<< HEAD
 
 import initialState from '@/utils/gcbmInitialStoreState.js'
+=======
+>>>>>>> refactor: Migrate from Toasts to Notifications (#316)
 
 export default {
   state: {
