b83369ffea5c228a876a07fac593c4bfe4cfbfcf
YashKandalkar <yashsk.yk@gmail.com>
2022-06-23 17:20:59 +0530

    feat: add Variables config

---
+++ flint.ui/src/components/GCBM/Configurations/Pools.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="py-4 px-8 text-earth">
+    <a-typography-title><span class="font-normal text-earth"> Configure Pools </span></a-typography-title>
+    <a-typography-text>
+      <span class="text-earth mb-6 block"> Configure the starting points for each pool. </span>
+    </a-typography-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pools',
+  setup() {
+    return {}
+  }
+}
+</script>
