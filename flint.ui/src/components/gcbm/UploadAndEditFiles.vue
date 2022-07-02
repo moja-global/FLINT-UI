5791a3f8ac4f1b7912e76dcec2dc39cf5dd51f60
YashKandalkar <yashsk.yk@gmail.com>
2022-07-02 21:50:12 +0530

    feat: add disturbances, input-db, misc. components, UI for showing uploaded files Signed-off-by: YashKandalkar <yashsk.yk@gmail.com>

--- flint.ui/src/components/gcbm/UploadAndEditFiles.vue
+++ flint.ui/src/components/gcbm/UploadAndEditFiles.vue
@@ -7,19 +7,49 @@
     @remove="handleRemove"
     @change="handleChange"
     @drop="handleDrop"
-    style="padding: 14px"
+    style="padding: 14px; margin-bottom: 28px"
   >
     <p class="ant-upload-drag-icon">
       <UploadOutlined />
     </p>
     <p class="ant-upload-text">Click or drag file to this area to upload</p>
     <p class="ant-upload-hint">Support for a single or bulk upload.</p>
+    <template #itemRender="{ file }">
+      <div
+        :class="`my-4 transition-all relative
+                 duration-100 p-2 rounded-sm flex flex-col
+                 gap-2 border-dashed hover:border-gray-400`"
+        style="border-width: 1px"
+        @mouseover="() => onFileMouseOver(file)"
+        @mouseout="() => onFileMouseOut(file)"
+      >
+        <div class="flex justify-between">
+          <a-typography-title
+            :level="5"
+            :style="[file.status === 'error' ? { color: 'red', margin: 0 } : { margin: 0 }]"
+          >
+            {{ file.uid }}
+          </a-typography-title>
+          <a-button v-show="fileHoveredUid === file.uid" type="text" class="right-0 top-0" style="position: absolute">
+            <template #icon>
+              <DeleteOutlined />
+            </template>
+          </a-button>
+        </div>
+        <div class="flex justify-between">
+          <a-typography-text type="secondary"> {{ bytesToKB(file.size) }}</a-typography-text>
+          <a-button type="primary">Preview Config</a-button>
+        </div>
+      </div>
+    </template>
   </a-upload-dragger>
 </template>
 
 <script>
 import { ref } from 'vue'
-import { UploadOutlined } from '@ant-design/icons-vue'
+import { UploadOutlined, DeleteOutlined } from '@ant-design/icons-vue'
+import useFunctions from '@/utils/useFunctions'
+
 export default {
   name: 'UploadAndEditFiles',
   props: {
@@ -28,10 +58,15 @@ export default {
     }
   },
   components: {
-    UploadOutlined
+    UploadOutlined,
+    DeleteOutlined
   },
   setup() {
     const fileList = ref([])
+    const fileHoveredUid = ref(null)
+
+    const { bytesToKB } = useFunctions()
+
     const handleChange = (info) => {
       const status = info.file.status
       if (status !== 'uploading') {
@@ -62,7 +97,26 @@ export default {
       console.log(fileList)
     }
 
-    return { fileList, handleChange, handleDrop, handleRemove, beforeUpload }
+    const onFileMouseOver = (file) => {
+      fileHoveredUid.value = file.uid
+      console.log(fileHoveredUid.value)
+    }
+
+    const onFileMouseOut = () => {
+      fileHoveredUid.value = null
+    }
+
+    return {
+      fileList,
+      bytesToKB,
+      fileHoveredUid,
+      handleChange,
+      handleDrop,
+      handleRemove,
+      beforeUpload,
+      onFileMouseOver,
+      onFileMouseOut
+    }
   }
 }
 </script>
