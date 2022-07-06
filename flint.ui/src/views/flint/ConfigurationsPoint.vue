6ded5d12592570cdcb71d4508ab6a66dd21e7049
Amit <amitkulkarni2028@gmail.com>
2022-07-06 15:15:17 +0530

    refactor: ConfigurationsPoint to composition API
    Signed-off-by: Amit <amitkulkarni2028@gmail.com>

--- flint.ui/src/views/flint/ConfigurationsPoint.vue
+++ flint.ui/src/views/flint/ConfigurationsPoint.vue
@@ -33,15 +33,15 @@
             <div class="">
               <div data-v-step="2">
                 <div class="text-earth flex gap-4 justify-between mb-6">
-                  <span>Pool 1: {{ pool1.value }}</span>
+                  <span>Pool 1: {{ pool1.pool_value }}</span>
                   <span>{{ pool1.max }}</span>
                 </div>
                 <Slider
-                  :value="pool1.value"
+                  :value="pool1.pool_value"
                   :max="pool1.max"
                   :min="pool1.min"
                   :step="pool1.step"
-                  @changeVal="(val) => (pool1.value = val)"
+                  @changeVal="(val) => (pool1.pool_value = val)"
                 />
               </div>
             </div>
@@ -51,15 +51,15 @@
             <div class="">
               <div data-v-step="3">
                 <div class="text-earth flex gap-4 justify-between mb-6">
-                  <span>Pool 2: {{ pool2.value }}</span>
+                  <span>Pool 2: {{ pool2.pool_value }}</span>
                   <span>{{ pool2.max }}</span>
                 </div>
                 <Slider
-                  :value="pool2.value"
+                  :value="pool2.pool_value"
                   :max="pool2.max"
                   :min="pool2.min"
                   :step="pool2.step"
-                  @changeVal="(val) => (pool2.value = val)"
+                  @changeVal="(val) => (pool2.pool_value = val)"
                 />
               </div>
             </div>
@@ -69,15 +69,15 @@
             <div class="">
               <div data-v-step="4">
                 <div class="text-earth flex gap-4 justify-between mb-6">
-                  <span>Pool 3: {{ pool3.value }}</span>
+                  <span>Pool 3: {{ pool3.pool_value }}</span>
                   <span>{{ pool3.max }}</span>
                 </div>
                 <Slider
-                  :value="pool3.value"
+                  :value="pool3.pool_value"
                   :max="pool3.max"
                   :min="pool3.min"
                   :step="pool3.step"
-                  @changeVal="(val) => (pool3.value = val)"
+                  @changeVal="(val) => (pool3.pool_value = val)"
                 />
               </div>
             </div>
@@ -104,6 +104,9 @@ import Maptest from '@/components/Vuelayers/Maptest.vue'
 import Slider from '@/components/Slider/Slider.vue'
 import PointOuterTable from './PointOuterTable.vue'
 
+import { ref, onMounted, getCurrentInstance } from 'vue'
+import { useStore } from 'vuex'
+
 export default {
   components: {
     Button,
@@ -112,110 +115,128 @@ export default {
     Slider,
     PointOuterTable
   },
-  data() {
-    return {
-      isShow: false,
-      showTable: false,
 
-      pool1: {
-        min: 0.0,
-        max: 400.0,
-        interval: 5.0,
-        value: 100.0
-      },
-      pool2: {
-        min: 0.0,
-        max: 400.0,
-        interval: 5.0,
-        value: 100.0
-      },
-      pool3: {
-        min: 0.0,
-        max: 400.0,
-        interval: 5.0,
-        value: 100.0
-      },
-      myOptions: {
-        useKeyboardNavigation: true,
-        labels: {
-          buttonSkip: 'Skip tour',
-          buttonPrevious: 'Previous',
-          buttonNext: 'Next',
-          buttonStop: 'Finish'
+  setup() {
+    const isShow = ref(false)
+    const showTable = ref(false)
+
+    const store = useStore()
+
+    const pool1 = ref({
+      min: 0.0,
+      max: 400.0,
+      interval: 5.0,
+      pool_value: 100.0
+    })
+
+    const pool2 = ref({
+      min: 0.0,
+      max: 400.0,
+      interval: 5.0,
+      pool_value: 100.0
+    })
+
+    const pool3 = ref({
+      min: 0.0,
+      max: 400.0,
+      interval: 5.0,
+      pool_value: 100.0
+    })
+
+    const myOptions = ref({
+      useKeyboardNavigation: true,
+      labels: {
+        buttonSkip: 'Skip tour',
+        buttonPrevious: 'Previous',
+        buttonNext: 'Next',
+        buttonStop: 'Finish'
+      }
+    })
+
+    const steps = [
+      {
+        target: '[data-v-step="1"]',
+        content: 'Set the Start and End date of simulation from the date picker.',
+        params: {
+          placement: 'bottom'
         }
       },
-      steps: [
-        {
-          target: '[data-v-step="1"]',
-          content: 'Set the Start and End date of simulation from the date picker.',
-          params: {
-            placement: 'bottom'
-          }
-        },
-        {
-          target: '[data-v-step="2"]',
-          content: 'Use the slider to set the Pool 1 value.',
-          params: {
-            placement: 'top'
-          }
-        },
-        {
-          target: '[data-v-step="3"]',
-          content: 'Use the slider to set the Pool 2 value.',
-          params: {
-            placement: 'top'
-          }
-        },
-        {
-          target: '[data-v-step="4"]',
-          content: 'Use the slider to set the Pool 3 value.',
-          params: {
-            placement: 'top'
-          }
-        },
-        {
-          target: '[data-v-step="5"]',
-          content: 'Click on the RUN button to start the simulation.',
-          params: {
-            placement: 'top'
-          }
-        },
-        {
-          target: '[data-v-step="6"]',
-          content: 'Click on this button to see the Point output.',
-          params: {
-            placement: 'top'
-          }
+      {
+        target: '[data-v-step="2"]',
+        content: 'Use the slider to set the Pool 1 value.',
+        params: {
+          placement: 'top'
         }
-      ]
-    }
-  },
-  mounted: function () {
-    this.$tours['MyTour'].start()
-  },
-  methods: {
-    finalPoolValues() {
-      if (
-        this.pool1.value.toFixed(1) === this.pool2.value.toFixed(1) &&
-        this.pool2.value.toFixed(1) === this.pool3.value.toFixed(1)
-      ) {
-        console.log('Config values not changed!!')
+      },
+      {
+        target: '[data-v-step="3"]',
+        content: 'Use the slider to set the Pool 2 value.',
+        params: {
+          placement: 'top'
+        }
+      },
+      {
+        target: '[data-v-step="4"]',
+        content: 'Use the slider to set the Pool 3 value.',
+        params: {
+          placement: 'top'
+        }
+      },
+      {
+        target: '[data-v-step="5"]',
+        content: 'Click on the RUN button to start the simulation.',
+        params: {
+          placement: 'top'
+        }
+      },
+      {
+        target: '[data-v-step="6"]',
+        content: 'Click on this button to see the Point output.',
+        params: {
+          placement: 'top'
+        }
+      }
+    ]
+
+    function finalPoolValues() {
+      if (pool1.value.pool_value === 100 && pool2.value.pool_value === 100 && pool3.value.pool_value === 100) {
+        console.log('Pool values are probably unchanged!')
       }
 
-      this.$store.commit('set_pointConfig_Pool_1', this.pool1.value.toFixed(1))
-      this.$store.commit('set_pointConfig_Pool_2', this.pool2.value.toFixed(1))
-      this.$store.commit('set_pointConfig_Pool_3', this.pool3.value.toFixed(1))
+      store.commit('set_pointConfig_Pool_1', pool1.value.pool_value.toFixed(1))
+      store.commit('set_pointConfig_Pool_2', pool2.value.pool_value.toFixed(1))
+      store.commit('set_pointConfig_Pool_3', pool3.value.pool_value.toFixed(1))
 
-      this.$store.dispatch('send_pointConfig')
-    },
+      store.dispatch('send_pointConfig')
+    }
 
-    Run() {
-      this.finalPoolValues() //This does whatever the stepper does.
-      this.showTable = false
-    },
+    function Run() {
+      finalPoolValues()
+      showTable.value = false
+    }
 
-    showPointOutputTable() {
-      this.showTable = true
+    function showPointOutputTable() {
+      showTable.value = true
+    }
+
+    onMounted(() => {
+      // https://github.com/pulsardev/vue-tour/issues/166#issuecomment-1064000704
+      const internalInstance = getCurrentInstance()
+      const tours = internalInstance.appContext.config.globalProperties.$tours
+      tours['MyTour'].start()
+    })
+
+    return {
+      isShow,
+      showTable,
+      pool1,
+      pool2,
+      pool3,
+      myOptions,
+      steps,
+      finalPoolValues,
+      Run,
+      showPointOutputTable
     }
   }
 }
