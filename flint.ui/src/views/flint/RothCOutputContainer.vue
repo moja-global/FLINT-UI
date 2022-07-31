96e03c20cdfe5335170ce767aa3ba5775d7afe9a
Karan Singh Thakur <karanthakur.work@gmail.com>
2022-07-31 23:05:19 +0530

    fix: refractoring issues

--- flint.ui/src/views/flint/RothCOutputContainer.vue
+++ flint.ui/src/views/flint/RothCOutputContainer.vue
@@ -12,19 +12,125 @@
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { ref } from 'vue'
 import RothCOutput from './RothCOutput.vue'
 import Table from './Table.vue'
 import dayjs from 'dayjs'
+import { useStore } from 'vuex'
+
 
 export default {
-  title: 'RothCOuterContainer',
+  name: 'RothCOutputContainer',
   components: {
     RothCOutput,
     Table
   },
   setup() {
-    const coloumns = reactive([
+    const store = useStore()
+    const isTable = ref(true)
+
+    function getRothC_step() {
+      console.log('store.state.rothc.RothC_step')
+      console.log(this.store.state.rothc.RothC_step)
+      return store.state.rothc.RothC_step
+    }
+    function getRothC_stepDate() {
+      console.log('store.state.rothc.RothC_stepDate')
+      console.log(store.state.rothc.RothC_stepDate)
+      return store.state.rothc.RothC_stepDate
+    }
+    function getDPM() {
+      console.log('store.state.rothc.DPM')
+      console.log(store.state.rothc.DPM)
+      return store.state.rothc.DPM
+    }
+    function getRPM() {
+      console.log('store.state.rothc.RPM')
+      console.log(store.state.rothc.RPM)
+      return store.state.rothc.RPM
+    }
+    function getBIOF() {
+      console.log('store.state.rothc.BIOF')
+      console.log(store.state.rothc.BIOF)
+      return store.state.rothc.BIOF
+    }
+    function getBIOS() {
+      console.log('store.state.rothc.BIOS')
+      console.log(store.state.rothc.BIOS)
+      return store.state.rothc.BIOS
+    }
+    function getHUM() {
+      console.log('store.state.rothc.HUM')
+      console.log(store.state.rothc.HUM)
+      return store.state.rothc.HUM
+    }
+    function getIOM() {
+      console.log('store.state.rothc.IOM')
+      console.log(store.state.rothc.IOM)
+      return store.state.rothc.IOM
+    }
+    function getplantCM() {
+      console.log('store.state.rothc.plantCM')
+      console.log(store.state.rothc.plantCM)
+      return store.state.rothc.plantCM
+    }
+    function getatmosphere() {
+      console.log('store.state.rothc.atmosphere')
+      console.log(store.state.rothc.atmosphere)
+      return store.state.rothc.atmosphere
+    }
+    function generateDataRows() {
+      var result = []
+      var DPM = getDPM(),
+        RPM = getRPM(),
+        BIOF = getBIOF(),
+        BIOS = getBIOS(),
+        HUM = getHUM(),
+        IOM = getIOM(),
+        RothC_step = getRothC_step(),
+        RothC_stepDate = getRothC_stepDate(),
+        plantCM = getplantCM(),
+        atmosphere = getatmosphere()
+      console.log('DPM from generateDataRows')
+      console.log(DPM)
+      console.log('RPM from generateDataRows')
+      console.log(RPM)
+      console.log('BIOF from generateDataRows')
+      console.log(BIOF)
+      console.log('BIOS from generateDataRows')
+      console.log(BIOS)
+      console.log('HUM from generateDataRows')
+      console.log(HUM)
+      console.log('IOM from generateDataRows')
+      console.log(IOM)
+      console.log('plantCM from generateDataRows')
+      console.log(plantCM)
+      console.log('atmosphere from generateDataRows')
+      console.log(atmosphere)
+      for (let j = 0; j < RothC_step.length; j++) {
+        let row = j
+        if (!result[row]) {
+          result[row] = {}
+        }
+        result[row]['RothC_step'] = RothC_step[row]
+        var defaultDateFormat = new Date(dayjs(RothC_stepDate[row], 'YYYY-MM-DD:HH:mm:ss.SSSSSS'))
+        result[row]['RothC_stepDate'] = defaultDateFormat.toISOString().substring(0, 10)
+        result[row]['DPM'] = DPM[row].toPrecision(5)
+        result[row]['RPM'] = RPM[row].toPrecision(5)
+        result[row]['BIOF'] = BIOF[row].toPrecision(5)
+        result[row]['BIOS'] = BIOS[row].toPrecision(5)
+        result[row]['HUM'] = HUM[row].toPrecision(5)
+        result[row]['IOM'] = IOM[row].toPrecision(5)
+        result[row]['plantCM'] = plantCM[row].toPrecision(5)
+        result[row]['atmosphere'] = atmosphere[row].toPrecision(5)
+      }
+      console.log('result')
+      console.log(result)
+      return result
+    }
+
+    const rows = generateDataRows()
+    const columns = ref([
       {
         dataIndex: 'RothC_step',
         key: 'RothC_step',
@@ -76,121 +182,22 @@ export default {
         title: 'atmosphere'
       }
     ])
-    const rows = generateDataRows()
 
-    const RothC_step = () => {
-      console.log('this.store.state.rothc.RothC_step')
-      console.log(this.store.state.rothc.RothC_step)
-      return this.store.state.rothc.RothC_step
-    }
-    const RothC_stepDate = () => {
-      console.log('this.store.state.rothc.RothC_stepDate')
-      console.log(this.store.state.rothc.RothC_stepDate)
-      return this.store.state.rothc.RothC_stepDate
-    }
-    const DPM = () => {
-      console.log('this.store.state.rothc.DPM')
-      console.log(this.store.state.rothc.DPM)
-      return this.store.state.rothc.DPM
-    }
-    const RPM = () => {
-      console.log('this.store.state.rothc.RPM')
-      console.log(this.store.state.rothc.RPM)
-      return this.store.state.rothc.RPM
-    }
-    const BIOF = () => {
-      console.log('this.store.state.rothc.BIOF')
-      console.log(this.store.state.rothc.BIOF)
-      return this.store.state.rothc.BIOF
-    }
-    const BIOS = () => {
-      console.log('this.store.state.rothc.BIOS')
-      console.log(this.store.state.rothc.BIOS)
-      return this.store.state.rothc.BIOS
-    }
-    const HUM = () => {
-      console.log('this.store.state.rothc.HUM')
-      console.log(this.store.state.rothc.HUM)
-      return this.store.state.rothc.HUM
-    }
-    const IOM = () => {
-      console.log('this.store.state.rothc.IOM')
-      console.log(this.store.state.rothc.IOM)
-      return this.store.state.rothc.IOM
-    }
-    const plantCM = () => {
-      console.log('this.store.state.rothc.plantCM')
-      console.log(this.store.state.rothc.plantCM)
-      return this.store.state.rothc.plantCM
-    }
-    const atmosphere = () => {
-      console.log('this.store.state.rothc.atmosphere')
-      console.log(this.store.state.rothc.atmosphere)
-      return this.store.state.rothc.atmosphere
-    }
-    const generateDataRows = () => {
-      var result = []
-      var DPM = this.DPM(),
-        RPM = this.RPM(),
-        BIOF = this.BIOF(),
-        BIOS = this.BIOS(),
-        HUM = this.HUM(),
-        IOM = this.IOM(),
-        RothC_step = this.RothC_step(),
-        RothC_stepDate = this.RothC_stepDate(),
-        plantCM = this.plantCM(),
-        atmosphere = this.atmosphere()
-      console.log('DPM from generateDataRows')
-      console.log(DPM)
-      console.log('RPM from generateDataRows')
-      console.log(RPM)
-      console.log('BIOF from generateDataRows')
-      console.log(BIOF)
-      console.log('BIOS from generateDataRows')
-      console.log(BIOS)
-      console.log('HUM from generateDataRows')
-      console.log(HUM)
-      console.log('IOM from generateDataRows')
-      console.log(IOM)
-      console.log('plantCM from generateDataRows')
-      console.log(plantCM)
-      console.log('atmosphere from generateDataRows')
-      console.log(atmosphere)
-      for (let j = 0; j < RothC_step.length; j++) {
-        let row = j
-        if (!result[row]) {
-          result[row] = {}
-        }
-        result[row]['RothC_step'] = RothC_step[row]
-        var defaultDateFormat = new Date(dayjs(RothC_stepDate[row], 'YYYY-MM-DD:HH:mm:ss.SSSSSS'))
-        result[row]['RothC_stepDate'] = defaultDateFormat.toISOString().substring(0, 10)
-        result[row]['DPM'] = DPM[row].toPrecision(5)
-        result[row]['RPM'] = RPM[row].toPrecision(5)
-        result[row]['BIOF'] = BIOF[row].toPrecision(5)
-        result[row]['BIOS'] = BIOS[row].toPrecision(5)
-        result[row]['HUM'] = HUM[row].toPrecision(5)
-        result[row]['IOM'] = IOM[row].toPrecision(5)
-        result[row]['plantCM'] = plantCM[row].toPrecision(5)
-        result[row]['atmosphere'] = atmosphere[row].toPrecision(5)
-      }
-      console.log('result')
-      console.log(result)
-      return result
-    }
     return {
-      isTable: true,
-      coloumns,
+      isTable,
+      columns,
       rows,
-      RothC_step,
-      RothC_stepDate,
-      DPM,
-      RPM,
-      BIOF,
-      BIOS,
-      HUM,
-      IOM,
-      plantCM,
-      atmosphere
+      getRothC_step,
+      getRothC_stepDate,
+      getDPM,
+      getRPM,
+      getBIOF,
+      getBIOS,
+      getHUM,
+      getIOM,
+      getplantCM,
+      getatmosphere,
+      generateDataRows
     }
   }
 }
