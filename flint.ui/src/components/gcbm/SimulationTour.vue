c5423a6984ccaafa0d573c2f026a8a47835197f4
YashKandalkar <yashsk.yk@gmail.com>
2022-09-01 22:36:14 +0530

    feat: add SimulationTour, WIP. Signed-off-by: YashKandalkar <yashsk.yk@gmail.com>

---
+++ flint.ui/src/components/gcbm/SimulationTour.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-modal :visible="false" title="GCBM Simulation Editor">
+    <!-- <a-typography-title :level="4">Create Simulation</a-typography-title>
+    <a-divider style="margin: 0; margin-bottom: 16px" />
+    <div class="flex justify-center">
+      <a-image :src="require('@/assets/gcbm-upload-folder-structure.png')" />
+    </div> -->
+  </a-modal>
+</template>
+
+<script>
+export default {
+  name: 'SimulationTour',
+  setup() {
+    return {}
+  }
+}
+</script>
