3a4a97d01de42819998f774532f761a026b3cda2
Kamal Nayan <geniusamansingh@gmail.com>
2022-09-08 18:03:48 +0000

    fixing CI bug w/signoff
    Signed-off-by: Kamal Nayan <geniusamansingh@gmail.com>

--- flint.ui/src/views/flint/RothCOutputGraph.vue
+++ flint.ui/src/views/flint/RothCOutputGraph.vue
@@ -22,89 +22,89 @@ export default {
   setup() {
     const store = useStore()
     const series = ref([
-        {
-            name: 'Soil DPM',
-            data: this.$store.state.rothc.DPM
-        },
-        {
-            name: 'Soil RPM',
-            data: this.$store.state.rothc.RPM
-        },
-        {
-            name: 'Soil BioF',
-            data: this.$store.state.rothc.BIOF
-        },
-        {
-            name: 'Soil BioS',
-            data: this.$store.state.rothc.BIOS
-        },
-        {
-            name: 'Soil HUM',
-            data: this.$store.state.rothc.HUM
-        },
-        {
-            name: 'Soil IOM',
-            data: this.$store.state.rothc.IOM
-        }
+      {
+        name: 'Soil DPM',
+        data: this.$store.state.rothc.DPM
+      },
+      {
+        name: 'Soil RPM',
+        data: this.$store.state.rothc.RPM
+      },
+      {
+        name: 'Soil BioF',
+        data: this.$store.state.rothc.BIOF
+      },
+      {
+        name: 'Soil BioS',
+        data: this.$store.state.rothc.BIOS
+      },
+      {
+        name: 'Soil HUM',
+        data: this.$store.state.rothc.HUM
+      },
+      {
+        name: 'Soil IOM',
+        data: this.$store.state.rothc.IOM
+      }
     ])
     const chartOptionsArea = ref({
-        chart: {
-          id: 'chartArea',
-          toolbar: {
-            autoSelected: 'pan',
-            show: true
-          }
-        },
-        colors: ['#EF476F', '#FFD166', '#06D6A0', '#118AB2', '#073B4C', '#008b8b'],
-        stroke: {
-          width: 3
-        },
-        dataLabels: {
-          enabled: false
-        },
-        fill: {
-          opacity: 1
-        },
-        markers: {
-          size: 0
-        },
-        xaxis: {
-          type: 'numeric'
+      chart: {
+        id: 'chartArea',
+        toolbar: {
+          autoSelected: 'pan',
+          show: true
         }
+      },
+      colors: ['#EF476F', '#FFD166', '#06D6A0', '#118AB2', '#073B4C', '#008b8b'],
+      stroke: {
+        width: 3
+      },
+      dataLabels: {
+        enabled: false
+      },
+      fill: {
+        opacity: 1
+      },
+      markers: {
+        size: 0
+      },
+      xaxis: {
+        type: 'numeric'
+      }
     })
     const chartOptionsBrush = ref({
-        chart: {
-          id: 'chartBrush',
-          brush: {
-            target: 'chartArea',
-            enabled: true,
-            autoScaleYaxis: true
-          },
-          selection: {
-            enabled: true,
-            xaxis: {
-              min: 1,
-              max: 20
-            }
-          }
+      chart: {
+        id: 'chartBrush',
+        brush: {
+          target: 'chartArea',
+          enabled: true,
+          autoScaleYaxis: true
         },
-        colors: ['#008FFB'],
-        fill: {
-          gradient: {
-            enabled: true,
-            opacityFrom: 0.91,
-            opacityTo: 0.1
+        selection: {
+          enabled: true,
+          xaxis: {
+            min: 1,
+            max: 20
           }
-        },
-        xaxis: {
-          type: 'numeric',
-          tooltip: {
-            enabled: true
-          }
-        },
-        yaxis: {
-          tickAmount: 2
         }
+      },
+      colors: ['#008FFB'],
+      fill: {
+        gradient: {
+          enabled: true,
+          opacityFrom: 0.91,
+          opacityTo: 0.1
+        }
+      },
+      xaxis: {
+        type: 'numeric',
+        tooltip: {
+          enabled: true
+        }
+      },
+      yaxis: {
+        tickAmount: 2
+      }
     })
     onBeforeMount(() => {
       store.dispatch('parse_point_results')
@@ -117,6 +117,7 @@ export default {
   }
 }
 </script>
+
 <style>
 div.chart-wrapper {
   padding-top: 40px !important;
