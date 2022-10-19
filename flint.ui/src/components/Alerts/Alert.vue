bdb1f9ff7b33f278f638dac5caae386e8d6c3551
Ashish Kumar <ashish.kumar85297@gmail.com>
2022-10-19 20:37:20 +0530

    Revert "Replace current Notifications component with UI-library's Notificatio…"

--- flint.ui/src/components/Alerts/Alert.vue
+++ flint.ui/src/components/Alerts/Alert.vue
@@ -2,7 +2,7 @@
   <a-alert style="margin-top: 0.5rem; margin-bottom: 0.5rem" v-bind="$attrs" v-on="$listeners" />
 </template>
 <script>
-import { Alert } from '@moja-global/mojaglobal-ui'
+import { Alert } from 'ant-design-vue'
 
 export default {
   name: 'Alert', // optional
