f852295cf162342d80ed3fac27a4af3cf8bc1961
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-01-16 12:45:38 +0530

    feat: replaced card component in LandingPageCard.vue (#444)
    * feat: replaced card component in LandingPageCard.vue
    
    Signed-off-by: Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
    
    * Update LandingPageCard.vue
    
    Signed-off-by: Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
    
    * Update LandingPageCard.vue
    
    Signed-off-by: Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
    
    Signed-off-by: Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>

--- flint.ui/src/components/Cards/LandingPageCard.vue
+++ flint.ui/src/components/Cards/LandingPageCard.vue
@@ -1,45 +1,47 @@
 <template>
-  <a-col
-    type="flex"
-    align="center"
-    :span="24"
-    :md="{ span: 11 }"
-    :xl="{ span: 7 }"
-    class="card bg-white rounded shadow-lg"
-  >
-    <a-col>
-      <h2 class="text-earth text-2xl mb-2">
-        {{ title }}
-      </h2>
-      <p class="text-earth text-base leading-7 justify-text">
-        {{ description }}
-      </p>
-    </a-col>
+  <a-col display="inline-block">
+    <CardComponent
+      class="card bg-white rounded shadow-lg cover"
+      align="center"
+      :span="24"
+      :md="{ span: 11 }"
+      :xl="{ span: 7 }"
+      type="flex"
+    >
+      <a-col>
+        <h2 class="text-earth text-2xl mb-2">
+          {{ title }}
+        </h2>
+        <p class="text-earth text-base leading-7 justify-text">
+          {{ description }}
+        </p>
+      </a-col>
 
-    <a-row type="flex" justify="center">
-      <a-space>
-        <ButtonComponent
-          classname="primarywh"
-          BackgroundColor="#ffffff"
-          Color="#475447"
-          Padding="0.4rem 1.3rem"
-          MinWidth="100px"
-          FontSize="15px"
-          @click="onClick"
-          >Explore <span> →</span></ButtonComponent
-        >
-        <ButtonComponent
-          classname="primarywh"
-          BackgroundColor="#ffffff"
-          Color="#475447"
-          Padding="0.4rem 1.3rem"
-          MinWidth="100px"
-          FontSize="15px"
-          @click="goDocs"
-          >Docs <span> →</span></ButtonComponent
-        >
-      </a-space>
-    </a-row>
+      <a-row type="flex" justify="center">
+        <a-space>
+          <ButtonComponent
+            classname="primarywh"
+            BackgroundColor="#ffffff"
+            Color="#475447"
+            Padding="0.4rem 1.3rem"
+            MinWidth="100px"
+            FontSize="15px"
+            @click="onClick"
+            >Explore <span> →</span></ButtonComponent
+          >
+          <ButtonComponent
+            classname="primarywh"
+            BackgroundColor="#ffffff"
+            Color="#475447"
+            Padding="0.4rem 1.3rem"
+            MinWidth="100px"
+            FontSize="15px"
+            @click="goDocs"
+            >Docs <span> →</span></ButtonComponent
+          >
+        </a-space>
+      </a-row>
+    </CardComponent>
   </a-col>
 </template>
 
@@ -47,10 +49,12 @@
 import { ButtonComponent } from '@moja-global/mojaglobal-ui'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { CardComponent } from '@moja-global/mojaglobal-ui'
 
 export default {
   components: {
-    ButtonComponent
+    ButtonComponent,
+    CardComponent
   },
   props: {
     title: { type: String, required: true },
@@ -96,9 +100,50 @@ export default {
   text-align: justify;
 }
 
-@media screen and (min-width: 1400px) {
+.cover {
+  height: 100%;
+  width: 100%;
+}
+
+@media screen and (min-width: 1223px) {
   .card {
-    max-width: 350px;
+    max-width: 320px;
+  }
+  .card:first-child {
+    margin-right: auto;
+  }
+  .card:last-child {
+    margin-left: auto;
+  }
+  .card:nth-child(2) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media screen and (min-width: 1080px) and (max-width: 1224px) {
+  .card {
+    max-width: 420px;
+  }
+  .card:first-child {
+    margin-right: auto;
+  }
+  .card:last-child {
+    margin-left: auto;
+  }
+  .card:nth-child(2) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media screen and (min-width: 768px) and (max-width: 1080px) {
+  .card {
+    padding: 16px 8px;
+    transition: transform 0.3s;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
   .card:first-child {
     margin-right: auto;
